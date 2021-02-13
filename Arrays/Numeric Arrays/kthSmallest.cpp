a2376ec3ce6eaf92c4a7bc796c1acb24d5b310ac
armboi <mnjups30@gmail.com>
2021-02-13 23:12:08 +0530

    Kth Smallest in Array

--- Arrays/Numeric Arrays/kthSmallest.cpp
+++ Arrays/Numeric Arrays/kthSmallest.cpp
@@ -1,6 +1,24 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int randomPartition(arr[], l , r){
   
+}
+
+int kthSmallest(arr[], 0, n-1, k){
+  if(k>0 && k<=r-l+1){
+    int pos = randomPartition(arr, l, r);
+
+  }
+  return INT_MAX;
+}
+
+int main(){
+  int arr[1000];
+  int n,k;
+  cin>>n>>k;
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  cout<<"Kth samllest element in the array id : " <<kthSmallest(arr, 0, n-1, k);
 }
